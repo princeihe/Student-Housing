3da8a406d5c2a9fd6ff1e5445d6f577a6208fb9d
Prince <91561054+princeihe@users.noreply.github.com>
2023-04-20 23:28:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Student Residence Management</title>
 </head>
 <body>
     <h1>Student Residence Management</h1>
-
+    <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#houses">Houses</a>
+        <a href="#contact">Contact</a>
+        <a href="about.html">About</a>
+        </div>
+    </div>
     <?php 
     // Database credentials
     $servername = "localhost";
@@ -17,33 +24,14 @@
     $dbname = "product";
 
     // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    $conn = mysqli_connect($servername, $username, $password, $housing);
 
     // Check connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
-    echo "Connected successfully";
-
-    // Select all records from User table
-    $sql = "SELECT * FROM User";
-    $result = mysqli_query($conn, $sql);
-
-    // Check if there are any records
-    if (mysqli_num_rows($result) > 0) {
-        // Output data of each row
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "User ID: " . $row["UserID"] . "<br>";
-            echo "Username: " . $row["Username"] . "<br>";
-            echo "First name: " . $row["FirstName"] . "<br>";
-            echo "Last name: " . $row["LastName"] . "<br>";
-            echo "<br>";
-        }
-    } else {
-        echo "0 results";
-    }
-
     ?>
+    
     <div>
         <ul><a href = "viewhouse.php">View houses currently in the database</a></ul>
         <ul><a href = "addhouse.php"> Add new house to the database</a></ul>
