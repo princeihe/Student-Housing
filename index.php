5bc0f9a59c8860d4ce47e5c85b653b9acb997e4c
Prince <91561054+princeihe@users.noreply.github.com>
2023-04-21 15:44:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
+<?php
+// Database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "housing";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Check if the form has been submitted
+if (isset($_POST["login"])) {
+    // Get the form data
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    // Execute SELECT query to check if user exists in the database
+    $sql = "SELECT * FROM User WHERE email='$email' AND password='$password'";
+
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        echo "Login successful.";
+        // Redirect to the house.php page
+        header("Location: home.html");
+        exit();
+    } else {
+        echo "Invalid email or password.";
+    }
+}
+
+// Close connection
+mysqli_close($conn);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,38 +45,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Student Residence Management</title>
+    <title>Login Form</title>
 </head>
 <body>
-    <h1>Student Residence Management</h1>
-    <div class="topnav">
-        <a class="active" href="#home">Home</a>
-        <a href="#houses">Houses</a>
-        <a href="#contact">Contact</a>
-        <a href="about.html">About</a>
-        </div>
+    <div class="login-form">
+        <h2>Login</h2>
+        <form method="post">
+            <label for="email">Email:</label>
+            <input type="text" name="email" required>
+            <br><br>
+            <label for="password">Password:</label>
+            <input type="password" name="password" required>
+            <br><br>
+            <input type="submit" name="login" value="Login">
+        </form>
+        <button><a href="register.php">Register</a><button>
     </div>
-    <?php 
-    // Database credentials
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "product";
 
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $housing);
-
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    ?>
-    
-    <div>
-        <ul><a href = "viewhouse.php">View houses currently in the database</a></ul>
-        <ul><a href = "addhouse.php"> Add new house to the database</a></ul>
-        <ul><a href = "edithouse.php">Edit house in the database</a></ul>
-        <ul><a href = "deletehouse.php">Delete a house from the database</a></ul>
-    </div>
+    <h1 class="testing">HELLO WORLD<h1>
 </body>
 </html>
