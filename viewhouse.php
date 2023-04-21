db9ca1075c5bf4ddb0ed45e788d8963103a415da
Prince <91561054+princeihe@users.noreply.github.com>
2023-04-21 15:45:54 +0100

    Update viewhouse.php

--- viewhouse.php
+++ viewhouse.php
@@ -1,115 +1,61 @@
-.add {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    border: solid black;
 
-    box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
 
-.test {
-    font-size: xx-large;
-}
+    <div class="topnav">
+            <a href="home.html">Home</a>
+            <a class="active" href="house.php">Houses</a>
+            <a href="contact.html">Contact</a>
+            <a href="about.html">About</a>
+    </div>
 
-.topnav {
-    overflow: hidden;
-    background-color: #333;
-}
-  
-.topnav a {
-    float: left;
-    color: #f2f2f2;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 17px;
-}
-  
-.topnav a:hover {
-    background-color: #ddd;
-    color: black;
-}
-  
-.topnav a.active {
-    background-color: grey;
-    color: white;
-}
+    <?php
+    // Database credentials
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "housing";
 
-body {
-    background-image: url('your-background-image.jpg');
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-  }
-  
-  .login-form {
-    width: 400px;
-    margin: auto;
-    margin-top: 100px;
-    padding: 20px;
-    background-color: #fff;
-    border-radius: 5px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
-  }
-  
-  .login-form h2 {
-    text-align: center;
-    margin-bottom: 20px;
-    color: #444;
-  }
-  
-  .login-form label {
-    display: block;
-    font-size: 16px;
-    color: #666;
-    margin-bottom: 5px;
-  }
-  
-  .login-form input[type="text"],
-  .login-form input[type="password"] {
-    width: 100%;
-    padding: 10px;
-    font-size: 16px;
-    border: none;
-    border-radius: 5px;
-    margin-bottom: 20px;
-    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
-  }
-  
-  .login-form input[type="submit"] {
-    width: 100%;
-    padding: 10px;
-    font-size: 16px;
-    border: none;
-    border-radius: 5px;
-    background-color: #428bca;
-    color: #fff;
-    cursor: pointer;
-  }
-  
-  .login-form input[type="submit"]:hover {
-    background-color: #3071a9;
-  }
-  
-  .login-form button {
-    display: block;
-    width: 100%;
-    padding: 10px;
-    font-size: 16px;
-    border: none;
-    border-radius: 5px;
-    background-color: #eee;
-    color: #444;
-    cursor: pointer;
-    text-align: center;
-  }
-  
-  .login-form button:hover {
-    background-color: #ccc;
-  }
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-  .testing h1 {
-    color: blue;
-  }
-  
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Execute SELECT query to retrieve all products
+    $sql = "SELECT * FROM Houses";
+    $result = mysqli_query($conn, $sql);
+
+    // Check if there are any records
+    if (mysqli_num_rows($result) > 0) {
+        // Output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<p>";
+            echo "<b>Location:</b> " . $row["location"] . "<br>";
+            echo "<b>Rooms:</b> " . $row["rooms"] . "<br>";
+            echo "<b>Size:</b> " . $row["size"] . "<br>";
+            echo "<b>Furnished:</b> " . $row["furnished"] . "<br>";
+            echo "<b>Parking:</b> " . $row["parking"] . "<br>";
+            echo "<b>Price:</b> " . $row["price"] . "<br>";
+            echo "<b>ID:</b> " . $row["id"] . "<br>";
+            echo "</p>";
+        }
+    } else {
+        echo "No houses found.";
+    }
+
+    // Close connection
+    mysqli_close($conn);
+    ?>
+</body>
+</html>
